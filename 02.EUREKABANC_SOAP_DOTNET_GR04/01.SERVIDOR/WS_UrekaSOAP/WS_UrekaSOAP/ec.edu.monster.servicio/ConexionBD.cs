b21124201e35d523246ea599a055afd7c1aa4333
adalicarvajal <lacarvajal4@espe.edu.ec>
2025-05-25 13:45:39 -0500

    servicio

---
+++ 02.EUREKABANC_SOAP_DOTNET_GR04/01.SERVIDOR/WS_UrekaSOAP/WS_UrekaSOAP/ec.edu.monster.servicio/ConexionBD.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+
+
+namespace WS_UrekaSOAP.ec.edu.monster.servicio
+{
+    public class ConexionBD
+    {
+        private readonly string connectionString = "Server=DESKTOP-GM48FCK;Database=eureka;User Id=sa;Password=Adali020;TrustServerCertificate=True;";
+
+        public SqlConnection GetConnection()
+        {
+            return new SqlConnection(connectionString);
+        }
+    }
+}
